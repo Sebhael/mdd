cafa8c62b7220ba9833bc529fd7137b0a7dde7e0
Kyle Gagliardo <sebhael@fullsail.edu>
2013-03-20 18:26:11 -0500

    Listing View
    Access by clicking "lists" on main page.

---
+++ application/views/task/list.php
@@ -0,0 +1,30 @@
+<style>
+#back-btn { display: block; }
+</style>
+<div class="content">
+	<ul data-role="listview" data-inset="true">
+    <li data-role="list-divider">Open Tasks <span class="ui-li-count">2</span></li>
+
+    <li><a href="index.html">
+        <h2>TaskManager: Facebook Sessioning</h2>
+        <p>Should probably get the Facebook library to actually push to a session, so that API is being used, eh?</p>
+        <p class="ui-li-aside">Due: <strong>11:00</strong>PM</p>
+    </a></li>
+    <li><a href="index.html">
+        <h2>TaskManager: Task CRUD</h2>
+        <p>I think the point of TaskManager is to create tasks...which isn't possible right now, working on it.</p>
+        <p class="ui-li-aside">Due: <strong>11:00</strong>AM</p>
+    </a></li>
+
+    <li data-role="list-divider">Recently Closed <span class="ui-li-count">1</span></li>
+
+    <li><a href="index.html">
+        <h2>TaskManager: Listing View</h2>
+        <img src="<?php echo base_url(); ?>test/task_ico.png" />
+        <p>Obviously complete, else you wouldn't be looking at this right now! Also has an icon, oooooh...</p>
+        <p class="ui-li-aside">Done: <strong>6:13</strong>PM</p>
+    </a></li>
+
+    <li><a href="index.html">Task Archive</a></li>
+</ul>
+</div>
